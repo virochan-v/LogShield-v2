ac1bc284e6ed29caa7574bb4515f8a2d9999f517
Virochan V <virochan.tech@gmail.com>
2026-07-22 09:32:13 +0530

    chore: initialize LogShield Spring Boot project

---
+++ src/main/java/com/logshield/logshieldv2/LogshieldV2Application.java
@@ -0,0 +1,13 @@
+package com.logshield.logshieldv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogshieldV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogshieldV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/logshield/logshieldv2/LogshieldV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logshield.logshieldv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogshieldV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
